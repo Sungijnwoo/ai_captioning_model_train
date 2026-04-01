8cdd74d76fbd9d9539ff70fda3fa54e27cfc5f04
tjdwlsdn333 <tjdwlsdn333@laonpeople.com>
2026-04-01 10:59:09 +0900

    Config dto 클래스 정의

---
+++ domain/dto/Config.py
@@ -0,0 +1,30 @@
+from dataclasses import asdict, dataclass
+
+import yaml
+
+
+@dataclass
+class Config:
+    clip_model: str
+    clip_model_path: str
+    llm_model: str
+    project_multi_rate: int
+    batch_size: int
+    max_text_length: int
+    align_epochs: int
+    caption_epochs: int
+    align_lr: float
+    caption_lr: float
+    train_samples: int
+    eval_samples: int
+    output_dir: str
+
+    @staticmethod
+    def load_config(path: str) -> "Config":
+        with open(path, "r", encoding="utf-8") as f:
+            data: dict = yaml.safe_load(f)
+        return Config(**data)
+
+    def __repr__(self) -> str:
+        data = asdict(self)
+        return yaml.dump(data, allow_unicode=True, sort_keys=False, default_flow_style=False, indent=4)
